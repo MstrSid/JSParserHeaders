3632af2189d106ad717afe34acf7b870da96a040
Eugene Kos <mstrsid@live.com>
2022-03-28 08:32:08 +0300

    first commit

---
+++ parser.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+	const body = document.querySelector('body');
+
+	let textNodes = [];
+
+	function recursy(element) {
+		element.childNodes.forEach(node => {
+			if (node.nodeName.match(/^H\d/g)) {
+				const obj = {
+					header: node.nodeName,
+					content: node.textContent,
+				};
+				textNodes.push(obj);
+			} else {
+				recursy(node);
+			}
+		});
+	}
+
+	recursy(body);
+
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		body: JSON.stringify(textNodes),
+	})
+	.then((response) => response.json())
+	.then((json) => console.log(json));
+});
